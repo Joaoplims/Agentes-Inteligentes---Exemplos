93992a2b768ef39e4b46ad2a8f5f542536c18bb8
Joaoplims <52757578+Joaoplims@users.noreply.github.com>
2025-04-07 17:15:00 -0300

    Update ActionNode.cs

--- Assets/Graphs/Scripts/DecisionTree/ActionNode.cs
+++ Assets/Graphs/Scripts/DecisionTree/ActionNode.cs
@@ -3,18 +3,15 @@ using UnityEngine;
 public class ActionNode: DecisionTreeNode
 {
     public string actionName; // Or any other action data you need
-
     public ActionNode(string name)
     {
         actionName = name;
     }
-
     public override DecisionTreeNode MakeDecision(AIBlackBoard blackboard)
     {
         // Actions simply return themselves
         return this;
     }
-
     public void Execute()
     {
         // Here you would implement the actual action logic
